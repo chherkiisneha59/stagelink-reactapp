daf54e7c536b2f133f816061cb3f98e43c8773bb
chherkiisneha59 <chherkiisneha@gmail.com>
2026-09-23 19:21:56 +0530

    Remove Welcome to StageLink heading from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to StageLink</h1>
             <p>Find the perfect artist for your event.</p>
         </div>
     );
 }
 
-export default Home;
+export default Home;
